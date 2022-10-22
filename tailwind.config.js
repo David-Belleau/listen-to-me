34c83fe928df5f9ee365c11a8b8fb34ee4273f87
David Belleau <david-belleau@hotmail.com>
2022-10-22 20:31:56 +0200

    feat/add fontFamily and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Helvetica', 'sans-serif']
+      },
+      colors: {
+        'orange': '#FFa900',
+        'white': '#F8F8F8',
+        'black': '#3F4345'
+      }
+    },
   },
   plugins: [],
 }
