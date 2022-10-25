215c043250e58919d8482941ced48c78ddddff98
David Belleau <david-belleau@hotmail.com>
2022-10-25 15:35:09 +0200

    feat/create file

---
+++ src/tests/NavBarMain.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import NavBarMain from "../components/navBarMain/NavBarMain";
+
+test('open dropdown "Mon compte"', () => {
+  render(<NavBarMain />);
+  const myAccount = screen.getByRole("button", { name: "Mon compte" });
+  expect(myAccount).toBeInTheDocument();
+  userEvent.click(myAccount);
+  expect(
+    screen.getByRole("link", { name: "Mes informations" })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole("link", { name: "Se déconnecter" })
+  ).toBeInTheDocument();
+});
