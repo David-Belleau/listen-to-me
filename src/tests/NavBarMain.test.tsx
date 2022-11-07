37328a82de0e706f702e9e2a8bc9fa76618888c6
David Belleau <david-belleau@hotmail.com>
2022-11-07 16:28:46 +0100

    test/add test

--- src/tests/NavBarMain.test.tsx
+++ src/tests/NavBarMain.test.tsx
@@ -2,6 +2,14 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import NavBarMain from "../components/navBarMain/NavBarMain";
 
+test("check elements presence",()=>{
+  render(<NavBarMain />);
+  expect(screen.getByRole('link', {name: 'logo du site'})).toBeInTheDocument()
+  expect(screen.getByRole('searchbox', {name: 'Search'})).toBeInTheDocument()
+  expect(screen.getByRole('link', {name: 'Panier'})).toBeInTheDocument()
+  expect(screen.getByRole("button", { name: "Mon compte" })).toBeInTheDocument();
+})
+
 test("open dropdown 'Mon compte'", () => {
   render(<NavBarMain />);
   const myAccount = screen.getByRole("button", { name: "Mon compte" });
