3aad7319d6e80d288b34962f665603e7a3032df7
David Belleau <david-belleau@hotmail.com>
2022-11-07 16:29:27 +0100

    test/add mock

--- src/tests/Products.test.tsx
+++ src/tests/Products.test.tsx
@@ -1,8 +1,38 @@
 import { render, screen } from "@testing-library/react";
 import Products from "../components/products/Products";
 
-test("check buttons presence 'Ajouter au panier' and 'Voir le détail'", () => {
-    render(<Products/>)
-    expect(screen.getByRole('link', {name: 'Ajouter au panier'})).toBeInTheDocument()
-    expect(screen.getByRole('link', {name: 'Voir le détail'})).toBeInTheDocument()
+jest.mock("../components/products/SliderTemplate.tsx", () => {
+  return jest.fn();
+});
+jest.mock("swiper", () => ({
+  Navigation: jest.fn(),
+  Pagination: jest.fn(),
+}));
+jest.mock("swiper/react", () => ({
+  SwiperSlide: jest.fn(),
+}));
+
+describe("check elements presence", () => {
+  test("check titles presence", () => {
+    render(<Products />);
+    expect(
+      screen.getByRole("heading", { name: "Artistes populaires" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: "Sons du moment" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: "Albums populaires" })
+    ).toBeInTheDocument();
+  });
+
+  test("check links presence 'Voir tous les morceaux' and 'Voir tous les albums'", () => {
+    render(<Products />);
+    expect(
+      screen.getByRole("link", { name: "Voir tous les morceaux" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", { name: "Voir tous les albums" })
+    ).toBeInTheDocument();
+  });
 });
