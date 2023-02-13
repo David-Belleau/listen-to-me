04feb3dabb6b5aba9ef39b69a78db105391bbe0b
David Belleau <david-belleau@hotmail.com>
2023-02-13 17:54:15 +0100

    style/delete console log

--- src/utils/spotifyApi.tsx
+++ src/utils/spotifyApi.tsx
@@ -16,14 +16,11 @@ const GetToken = async () => {
     },
     data: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`,
   });
-  console.log("getToken response.data", response.data);
   spotifyApi.setAccessToken(response.data.access_token);
   if (response.data.access_token) {
     return response.data.access_token;
   } else {
     const { status, statusText }: TokenResponseProps = response;
-    console.log("getToken status", status);
-    console.log("getToken statusText", statusText);
   }
 };
 
@@ -31,7 +28,6 @@ const GetToken = async () => {
 
 const GetMusicData = async () => {
   const token = await GetToken();
-  console.log("getMusic token", token);
   try {
     const { data } = await axios(`${uri}/browse/new-releases?limit=20`, {
       method: "GET",
@@ -41,10 +37,8 @@ const GetMusicData = async () => {
         Authorization: `Bearer ${token}`,
       },
     });
-    console.log("data", data?.albums);
     return data?.albums?.items;
   } catch (error) {
-    console.log("getMusic error", error);
     // error.response.status
     // error.message
   }
