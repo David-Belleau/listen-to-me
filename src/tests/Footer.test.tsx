37f04be18d4e095ab5cc1a83cb6e388d82d4fc3d
David Belleau <david-belleau@hotmail.com>
2023-02-13 17:55:00 +0100

    test/modify expect

--- src/tests/Footer.test.tsx
+++ src/tests/Footer.test.tsx
@@ -12,5 +12,5 @@ test("check elements presence",()=>{
     expect(screen.getByRole('img', {name:'logo de Youtube'})).toBeInTheDocument()
     expect(screen.getByRole('img', {name:'logo de Twitter'})).toBeInTheDocument()
     expect(screen.getByRole('img', {name:'logo de Instagram'})).toBeInTheDocument()
-    expect(screen.getByText('© 2022 Listen to me')).toBeInTheDocument()
+    expect(screen.getByText('© 2023 Listen to me')).toBeInTheDocument()
 })
