55b4bac06150a5dff7f137934f2c64742f77df0f
David Belleau <david-belleau@hotmail.com>
2023-03-04 20:16:32 +0100

    feat/delete file

--- src/tests/Unauth.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Unauth from "../Unauth";
-
-test('check elements presence',()=>{
-    render(<Unauth/>)
-    expect(screen.getByAltText('Logo du site')).toBeInTheDocument()
-    expect(screen.getByRole('button', {name:'Se connecter'})).toBeInTheDocument()
-})
