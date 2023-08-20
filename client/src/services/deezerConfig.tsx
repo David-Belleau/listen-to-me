bf6533669fce2e2667c7602ebc1a6656714abe03
David Belleau <david-belleau@hotmail.com>
2023-08-20 20:54:18 +0200

    feat/config api

---
+++ client/src/services/deezerConfig.tsx
@@ -0,0 +1,5 @@
+export const uri = process.env.REACT_APP_URI;
+export const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
+export const api_name = process.env.REACT_APP_API_NAME;
+export const app_id = process.env.REACT_APP_APP_ID;
+export const secret_key = process.env.REACT_SECRET_KEY;
