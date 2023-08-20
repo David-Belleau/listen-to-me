cffcab02dde59f0192a6f616d4a78b98b1414b56
David Belleau <david-belleau@hotmail.com>
2023-08-20 20:51:26 +0200

    feat/create types

---
+++ client/src/utils/types/components.tsx
@@ -0,0 +1,12 @@
+export type SliderProps = {
+  children: JSX.Element;
+};
+
+export type ProductProps = {
+  url?: string;
+  children?: string;
+};
+
+export type ProvidersProps = {
+  children: JSX.Element;
+};
