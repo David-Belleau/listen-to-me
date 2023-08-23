7f1b49d953e32d0a065e13854b314a0385cc2958
David Belleau <david-belleau@hotmail.com>
2023-08-23 13:27:45 +0200

    feat/add headers

--- netlify-functions/chart-artists.js
+++ netlify-functions/chart-artists.js
@@ -5,12 +5,20 @@ export const handler = async () => {
         const response = await axios?.get('https://api.deezer.com/chart/0/artists');
         return {
             statusCode: 200,
-            body: JSON.stringify(response?.data)
+            body: JSON.stringify(response?.data),
+            headers: {
+                "Access-Control-Allow-Origin": "https://listen-to-me.netlify.app",
+                "Access-Control-Allow-Headers": "Content-Type"
+            }
         };
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: error.message })
+            body: JSON.stringify({ error: error.message }),
+            headers: {
+                "Access-Control-Allow-Origin": "https://listen-to-me.netlify.app",
+                "Access-Control-Allow-Headers": "Content-Type"
+            }
         };
     }
 }
