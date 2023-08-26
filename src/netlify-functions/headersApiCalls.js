70d0265d857f562df4d10f3f52e9664801c68aad
David Belleau <david-belleau@hotmail.com>
2023-08-26 19:06:23 +0200

    feat/modify filename

---
+++ src/netlify-functions/headersApiCalls.js
@@ -0,0 +1,17 @@
+export const allowedOrigin = () => {
+    const origin = e => e.headers.Origin || e.headers.Origin
+    let allowedOrigin;
+
+    switch (origin()) {
+        case 'http://localhost:3000':
+            allowedOrigin = 'http://localhost:3000';
+            break;
+        case 'https://listen-to-me.netlify.app':
+            allowedOrigin = 'https://listen-to-me.netlify.app';
+            break;
+        default:
+            allowedOrigin = 'none';
+            break;
+    }
+    return allowedOrigin
+}
