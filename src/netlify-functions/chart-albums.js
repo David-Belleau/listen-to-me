221d2fbcc6f8d92c1cbe894cb4bd31946b93bb39
David Belleau <david-belleau@hotmail.com>
2023-09-10 18:05:51 +0200

    feat/modify headers

--- src/netlify-functions/chart-albums.js
+++ src/netlify-functions/chart-albums.js
@@ -1,13 +1,14 @@
 import axios from "axios"
+import { handler as allowedOrigin } from "./allowedOrigin";
 
-export const handler = async () => {
+export const handler = async (event) => {
     try {
         const response = await axios.get('https://api.deezer.com/chart/0/albums');
         return {
             statusCode: 200,
             body: JSON.stringify(response.data),
             headers: {
-                "Access-Control-Allow-Origin": '*',
+                "Access-Control-Allow-Origin": allowedOrigin(event),
                 "Access-Control-Allow-Headers": "Content-Type",
             }
         };
@@ -16,7 +17,7 @@ export const handler = async () => {
             statusCode: 500,
             body: JSON.stringify({ error: error.message }),
             headers: {
-                "Access-Control-Allow-Origin": '*',
+                "Access-Control-Allow-Origin": allowedOrigin(event),
                 "Access-Control-Allow-Headers": "Content-Type",
             }
         };
--- src/netlify-functions/chart-artists.js
+++ src/netlify-functions/chart-artists.js
@@ -1,13 +1,14 @@
 import axios from "axios"
+import { handler as allowedOrigin } from "./allowedOrigin";
 
-export const handler = async () => {
+export const handler = async (event) => {
     try {
         const response = await axios.get('https://api.deezer.com/chart/0/artists');
         return {
             statusCode: 200,
             body: JSON.stringify(response.data),
             headers: {
-                "Access-Control-Allow-Origin": '*',
+                "Access-Control-Allow-Origin": allowedOrigin(event),
                 "Access-Control-Allow-Headers": "Content-Type",
             }
         };
