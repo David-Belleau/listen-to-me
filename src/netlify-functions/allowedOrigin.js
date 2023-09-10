63ed7b154827f3a64028effacd9604d9cd1a317b
David Belleau <david-belleau@hotmail.com>
2023-09-10 14:43:18 +0200

    feat/modify switch

--- src/netlify-functions/allowedOrigin.js
+++ src/netlify-functions/allowedOrigin.js
@@ -1,17 +1,19 @@
 export const handler = (event) => {
-    const origin = event => event.headers.Origin || event.headers.Origin
+    const origin = event.headers.origin || event.headers.origin || 'undefined';
     let allowedOrigin;
 
-    switch (origin(event)) {
+    switch (origin) {
         case 'http://localhost:3000':
             allowedOrigin = 'http://localhost:3000';
             break;
         case 'https://listen-to-me.netlify.app':
             allowedOrigin = 'https://listen-to-me.netlify.app';
             break;
-        default:
-            allowedOrigin = 'none';
+        case 'undefined':
+            allowedOrigin = 'undefined';
             break;
+        default:
+            throw new Error(`Origin not allowed: ${origin}`);
     }
     return allowedOrigin
 }
