0f30d47201cf269a6a8bf4744fb20e1c49434fbf
David Belleau <david-belleau@hotmail.com>
2023-09-12 20:04:27 +0200

    feat/delete file

--- src/netlify-functions/ids-data.js
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom"
-
-const MusicDataId = (idData) => {
-    return idData = useParams()
-}
-
-export const handler = (idData) => {
-    return MusicDataId(idData)
-}
