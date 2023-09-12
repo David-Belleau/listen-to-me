c7490b2f91013b80f6c8de522b70a1625ea51e94
David Belleau <david-belleau@hotmail.com>
2023-09-12 20:05:52 +0200

    feat/create ternary

--- src/components/products/ArtistData.tsx
+++ src/components/products/ArtistData.tsx
@@ -2,14 +2,45 @@ import { Header } from "../header/Header";
 import { Contact } from "../contact/Contact";
 import { Footer } from "../footer/Footer";
 import { useGetArtistsDataQuery } from "../../services/deezerApiCalls";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Skeleton } from "../../shadcn/components/skeleton";
+import SliderTemplate from "./SliderTemplate";
+import { SwiperSlide } from "swiper/react";
 
 export const ArtistData = () => {
-  const { data, isLoading } = useGetArtistsDataQuery("artist data");
+  let artistId = useParams();
+  const { data: album, isLoading } = useGetArtistsDataQuery(artistId);
   return (
     <>
       <Header />
-      <div>albums et morceaux !</div>
+      {!album || isLoading === true ? (
+        <Skeleton />
+      ) : (
+        <SliderTemplate
+          children={
+            <>
+              {album?.data.map(
+                (album: {
+                  album: { title: string; cover_xl: string; id: number };
+                }) => (
+                  <SwiperSlide key={album.album.id}>
+                    <Link to="">
+                      <p className="text-center text-base sm:text-xl w-34 truncate">
+                        {album.album.title}
+                      </p>
+                      <img
+                        src={album.album.cover_xl}
+                        alt={album.album.title}
+                        className="w-48 mx-auto rounded"
+                      />
+                    </Link>
+                  </SwiperSlide>
+                )
+              )}
+            </>
+          }
+        />
+      )}
       <Contact />
       <Footer />
     </>
