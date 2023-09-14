53bc80e23a011fa711efad474c1f48ee510c5c09
David Belleau <david-belleau@hotmail.com>
2023-09-14 10:36:12 +0200

    feat/create constant

--- src/netlify-functions/artists-data.js
+++ src/netlify-functions/artists-data.js
@@ -1,11 +1,10 @@
 import axios from "axios"
 import { handler as allowedOrigin } from "./allowedOrigin";
-import { getIdData } from "../utils/functions";
 
-export const handler = async (event) => {
-    getIdData()
+export const handler = async (event,artistId) => {
+    const id = event?.pathParameters?.id || artistId
     try {
-        const response = await axios.get(`https://api.deezer.com/artist/${getIdData()}/top?limit=50`);
+        const response = await axios.get(`https://api.deezer.com/artist/${id}/top?limit=50`);
         return {
             statusCode: 200,
             body: JSON.stringify(response.data),
