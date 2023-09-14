9a8c4bba7bbf24e51fcfbeb7085944fdfbb09612
David Belleau <david-belleau@hotmail.com>
2023-09-14 10:33:02 +0200

    feat/modify filename

--- src/redux/features/getAccessTokenSlice.tsx
+++ src/redux/features/getAccessTokenSlice.tsx
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const authSlice = createSlice({
-  name: "authCode",
+const accessTokenSlice = createSlice({
+  name: "accessToken",
   initialState: "",
   reducers: {
-    addAuthCode: (state, action) => state + action.payload,
+    addAccessToken: (state, action) => state + action.payload,
   },
   /*  removeArticle: (state, action) => {
       return state.filter(article => article.id !== action.payload.id);
@@ -14,6 +14,6 @@ const authSlice = createSlice({
     } */
 });
 
-export const { addAuthCode /* , removeArticle, setArticles */ } =
-  authSlice.actions;
-export default authSlice.reducer;
+export const { addAccessToken /* , removeArticle, setArticles */ } =
+  accessTokenSlice.actions;
+export default accessTokenSlice.reducer;
