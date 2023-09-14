23b01d03ad851cf38d768fbd046d8adab146e6c5
David Belleau <david-belleau@hotmail.com>
2023-09-14 23:24:08 +0200

    style/modify className

--- src/components/products/Separator.tsx
+++ src/components/products/Separator.tsx
@@ -2,7 +2,7 @@ import { ProductProps } from "../../utils/types/components";
 
 export const Separator = ({children}: ProductProps) => {
   return (
-    <div className="relative h-1 bg-orange my-10 text-base sm:text-xl">
+    <div className="relative h-1 bg-orange my-10 text-base sm:text-xl text-center">
       <h2 className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-3">
         {children}
       </h2>
