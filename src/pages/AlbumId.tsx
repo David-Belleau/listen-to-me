42bf0cc5aa70e8aa8930ab01ab32765997dcce74
David Belleau <david-belleau@hotmail.com>
2023-09-15 23:37:28 +0200

    feat/import components

--- src/pages/AlbumId.tsx
+++ src/pages/AlbumId.tsx
@@ -1,9 +1,15 @@
+import { Contact } from "../components/contact/Contact";
+import { Footer } from "../components/footer/Footer";
+import { Header } from "../components/header/Header";
 import { AlbumData } from "../components/products/AlbumData";
 
 export const AlbumId = () => {
   return (
     <>
+      <Header />
       <AlbumData />
+      <Contact />
+      <Footer />
     </>
   );
 };
--- src/pages/TrackId.tsx
+++ src/pages/TrackId.tsx
@@ -1,8 +1,15 @@
 import { TrackData } from "../components/products/TrackData";
+import { Contact } from "../components/contact/Contact";
+import { Footer } from "../components/footer/Footer";
+import { Header } from "../components/header/Header";
+
 export const TrackId = () => {
   return (
     <>
+      <Header />
       <TrackData />
+      <Contact />
+      <Footer />
     </>
   );
 };
