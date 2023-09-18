cffab87a20e1a644d0e4de8e55f5c57a905d8e6c
David Belleau <david-belleau@hotmail.com>
2023-09-18 23:02:39 +0200

    feat/import icon + modify css

--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -1,16 +1,25 @@
-type AuthButtonProps = {
-  handleClick: () => void;
-};
+import { LuHeadphones } from "react-icons/lu";
+import type { AuthButtonProps } from "../../utils/types";
 
 export const AuthButton = ({ handleClick }: AuthButtonProps) => {
   return (
     <div
-      style={{ backgroundImage: `url('/images/bg_site.jpg')` }}
+      style={{
+        backgroundImage: `url('/images/bg_site.jpg')`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+      }}
       className="bg-cover bg-center bg-no-repeat h-screen"
       aria-label="bg-site"
     >
       <div className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 flex flex-col items-center gap-10">
-        <img src='/images/logo_site.png' alt="logo du site" className="w-48" />
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-orange h-12 border-4 rounded-full p-2">
+            <LuHeadphones className="text-orange" size={20} />
+          </div>
+          <p className="font-black text-2xl">Listen to me</p>
+        </div>
         <button
           className="rounded-lg m-auto bg-black text-white px-4 py-2 font-semibold cursor-pointer transition-all duration-400 ease-in-out hover:bg-orange focus:bg-neutral-200 focus:text-black"
           onClick={handleClick}
