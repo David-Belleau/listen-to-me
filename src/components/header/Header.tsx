0a0f5f41402182a5fb19ff1e726ac09acc879125
David Belleau <david-belleau@hotmail.com>
2023-09-18 23:03:51 +0200

    feat/create ternary

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,23 @@
-import {NavBarMain} from "../navBarMain/NavBarMain";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+import { ToggleBg, ToggleLogo } from '../../utils/darkMode';
 
 export const Header = () => {
+  const { isDarkMode, toggleMode } = ToggleBg();
+
   return (
-    <>
-      <NavBarMain />
-    </>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6">
+      <div className="flex items-center justify-between py-4 md:space-x-10">
+        <div className="flex justify-start">
+          <ToggleLogo/>
+        </div>
+        <button onClick={() => toggleMode()}>
+          {isDarkMode && isDarkMode === true ? (
+            <MdLightMode className="text-orange" size={30} title="light mode" aria-label="light mode"/>
+          ) : (
+            <MdDarkMode className="text-orange" size={30} title="dark mode"  />
+          )}
+        </button>
+      </div>
+    </div>
   );
 };
-
