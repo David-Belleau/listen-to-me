dff43c293776000fd51de0ac0fe566fc0531ce15
David Belleau <david-belleau@hotmail.com>
2023-09-18 23:10:20 +0200

    feat/create functions

---
+++ src/utils/darkMode.tsx
@@ -0,0 +1,78 @@
+import { LuHeadphones } from "react-icons/lu";
+import { useAppDispatch, useAppSelector } from "../redux/app/hooks";
+import { toggleDarkMode } from "../redux/features/darkModeSlice";
+import { RootState } from "./types";
+
+export const useDarkMode = () => {
+  const dispatch = useAppDispatch();
+  const isDarkMode = useAppSelector((state: RootState) => state.darkMode.value);
+
+  const toggleMode = () => {
+    // delete darkMode class in localStorage
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("darkMode", "false");
+    } else {
+      // add darkMode class in localStorage
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("darkMode", "true");
+    }
+    dispatch(toggleDarkMode());
+  };
+  return {
+    isDarkMode,
+    toggleMode,
+  };
+};
+
+export const ToggleBg = () => {
+  const { isDarkMode, toggleMode } = useDarkMode();
+  const toggleBg =
+    isDarkMode && isDarkMode === true ? "dark:bg-black" : "bg-white";
+  const toggleBgFooter =
+    isDarkMode && isDarkMode === true ? "dark:bg-black" : "bg-gray";
+  const toggleBgSeparator =
+    isDarkMode && isDarkMode === true ? "dark:bg-white" : "bg-black";
+  return {
+    isDarkMode,
+    toggleMode,
+    toggleBg,
+    toggleBgFooter,
+    toggleBgSeparator,
+  };
+};
+
+export const ToggleText = () => {
+  const { isDarkMode, toggleMode } = useDarkMode();
+  const toggleText =
+    isDarkMode && isDarkMode === true ? "dark:text-white" : "text-black";
+  return { isDarkMode, toggleMode, toggleText };
+};
+
+export const ToggleLogo = () => {
+  const { isDarkMode } = useDarkMode();
+  const { toggleText } = ToggleText();
+  return (
+    <>
+      {isDarkMode && isDarkMode === true ? (
+        <a href="/">
+          <div className="flex flex-col justify-center items-center w-30">
+            <div className="text-orange h-10 border-4 rounded-full p-2">
+              <LuHeadphones className="text-orange w-15" />
+            </div>
+            <p className={`font-black ${toggleText}`}>Listen to me</p>
+          </div>
+        </a>
+      ) : (
+        <a href="/">
+          <div className="flex flex-col justify-center items-center">
+            <div className="text-orange h-10 border-4 rounded-full p-2">
+              <LuHeadphones className="text-orange w-15" />
+            </div>
+            <p className="font-black">Listen to me</p>
+          </div>
+        </a>
+      )}
+    </>
+  );
+};
