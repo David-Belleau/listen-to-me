5e4d8a55b766267ad6990c7571fc3661ec3cdde5
David Belleau <david-belleau@hotmail.com>
2023-09-18 23:05:00 +0200

    feat/import function

--- src/components/products/Separator.tsx
+++ src/components/products/Separator.tsx
@@ -1,9 +1,12 @@
-import { ProductProps } from "../../utils/types/components";
+import { ToggleBg } from "../../utils/darkMode";
+import { ProductProps } from "../../utils/types";
 
 export const Separator = ({children}: ProductProps) => {
+  const { toggleBg } = ToggleBg();
+
   return (
-    <div className="relative h-1 bg-orange my-10 text-base sm:text-xl text-center">
-      <h2 className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-3">
+    <div className='relative h-1 bg-orange my-10 text-base sm:text-xl text-center'>
+      <h2 className={`absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-3 ${toggleBg}`}>
         {children}
       </h2>
     </div>
