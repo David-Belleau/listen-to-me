fba3a40bb59f3d71d0e2df0e5d262ff421bbe5a2
David Belleau <david-belleau@hotmail.com>
2023-09-20 18:29:42 +0200

    refactor/modify ternary

--- src/components/products/ProductsTemplate.tsx
+++ src/components/products/ProductsTemplate.tsx
@@ -1,9 +1,24 @@
+import { ProductsTemplateProps } from "@/src/utils/types";
 import { Skeleton } from "../loading/Skeleton";
+import { SeparatorTemplate } from "./SeparatorTemplate";
+import SliderTemplate from "./SliderTemplate";
 
-export const ProductsTemplate = (
-  products:{},
-  loading: boolean,
-  template: JSX.Element
-) => {
-  return <>{!products || loading === true ? <Skeleton /> : template}</>;
+export const ProductsTemplate = ({
+  title,
+  products,
+  loading,
+  template,
+}: ProductsTemplateProps) => {
+  return (
+    <>
+      <SeparatorTemplate children={title} />
+      {!products || loading === true ? (
+        <Skeleton />
+      ) : (
+        <>
+          <SliderTemplate children={template} />
+        </>
+      )}
+    </>
+  );
 };
