ee7a1f9f58e4956e6faf473b1d69b412a5d09a0e
David Belleau <david-belleau@hotmail.com>
2023-09-20 17:20:30 +0200

    feat/create function

---
+++ src/netlify-functions/template.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+import { api_name } from "../services/deezerConfig";
+import { handler as allowedOrigin } from "./allowedOrigin";
+
+export const handler = async (event,data) => {
+    try {
+        const response = await axios.get(`${api_name}chart/0/${data}`);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data),
+            headers: {
+                "Access-Control-Allow-Origin": allowedOrigin(event),
+                "Access-Control-Allow-Headers": "Content-Type",
+            }
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+            headers: {
+                "Access-Control-Allow-Origin": allowedOrigin(event),
+                "Access-Control-Allow-Headers": "Content-Type",
+            }
+        };
+    }
+}
