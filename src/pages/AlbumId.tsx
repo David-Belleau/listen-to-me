0b4040139d10d0332ebd222c65568a9df00286b0
David Belleau <david-belleau@hotmail.com>
2023-09-20 11:51:46 +0200

    feat/modify export

--- src/pages/AlbumId.tsx
+++ src/pages/AlbumId.tsx
@@ -3,7 +3,7 @@ import { Footer } from "../components/footer/Footer";
 import { Header } from "../components/header/Header";
 import { AlbumData } from "../components/products/AlbumData";
 
-export const AlbumId = () => {
+const AlbumId = () => {
   return (
     <>
       <Header />
@@ -13,3 +13,5 @@ export const AlbumId = () => {
     </>
   );
 };
+
+export default AlbumId;
