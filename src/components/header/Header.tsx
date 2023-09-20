143a0a71c03f7058737398a0063e7db783eb98e4
David Belleau <david-belleau@hotmail.com>
2023-09-20 11:49:42 +0200

    style/modify className

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   const { isDarkMode, toggleMode } = ToggleBg();
 
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6">
+    <div className="mx-auto max-w-full px-4 sm:px-6">
       <div className="flex items-center justify-between py-4 md:space-x-10">
         <div className="flex justify-start">
           <ToggleLogo/>
