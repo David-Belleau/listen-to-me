8b22b63f3e1feef6e8fb4bc5e400e35ddbe8c632
David Belleau <david-belleau@hotmail.com>
2023-09-20 11:52:06 +0200

    feat/create component

--- src/shadcn/components/skeleton.tsx
+++ src/shadcn/components/skeleton.tsx
@@ -1,5 +1,6 @@
 import { cn } from "../lib/utils";
 import React from "react";
+import { SiYoutubemusic } from "react-icons/si";
 
 const Template = ({
   className,
@@ -13,7 +14,8 @@ const Template = ({
   );
 };
 
-const Skeleton = () => {
+// loading components
+export const Skeleton = () => {
   return (
     <div className="flex justify-center space-x-4 py-4 px-1">
       <Template className="w-[50px] h-[50px] rounded-full bg-black" />
@@ -25,4 +27,13 @@ const Skeleton = () => {
   );
 };
 
-export { Skeleton };
+// loading app
+export const AppLoading = () => {
+  return (
+    <div className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+      <Template>
+        <SiYoutubemusic className="text-orange" size={90} />
+      </Template>
+    </div>
+  );
+};
