f985036561dfeb9d99bb9f91da83ea2121a2002d
David Belleau <david-belleau@hotmail.com>
2023-09-21 19:29:57 +0200

    feat/delete component + create function

--- src/utils/darkMode.tsx
+++ src/utils/darkMode.tsx
@@ -32,54 +32,32 @@ export const ToggleBg = () => {
     isDarkMode && isDarkMode === true ? "dark:bg-black" : "bg-white";
   const toggleBgFooter =
     isDarkMode && isDarkMode === true ? "dark:bg-black" : "bg-gray";
-  const toggleBgSeparator =
-    isDarkMode && isDarkMode === true ? "dark:bg-white" : "bg-black";
   return {
     isDarkMode,
     toggleMode,
     toggleBg,
     toggleBgFooter,
-    toggleBgSeparator,
   };
 };
 
 export const ToggleText = () => {
-  const { isDarkMode} = useDarkMode();
+  const { isDarkMode } = useDarkMode();
   const toggleText =
     isDarkMode && isDarkMode === true ? "dark:text-white" : "text-black";
   return { toggleText };
-};
-
-export const ToggleLogo = () => {
-  const { isDarkMode } = useDarkMode();
-  const { toggleText } = ToggleText();
-  return (
-    <>
-      {isDarkMode && isDarkMode === true ? (
-        <a href="/">
-          <div className="flex flex-col justify-center items-center w-30">
-            <div className="text-orange h-10 border-4 rounded-full p-2">
-              <LuHeadphones className="text-orange w-15" />
-            </div>
-            <p className={`font-black ${toggleText}`}>Listen to me</p>
-          </div>
-        </a>
-      ) : (
-        <a href="/">
-          <div className="flex flex-col justify-center items-center">
-            <div className="text-orange h-10 border-4 rounded-full p-2">
-              <LuHeadphones className="text-orange w-15" />
-            </div>
-            <p className="font-black">Listen to me</p>
-          </div>
-        </a>
-      )}
-    </>
-  );
 };
 
 export const ToggleWidget = () => {
   const { isDarkMode } = useDarkMode();
   const toggleWidget = isDarkMode && isDarkMode === true ? "dark" : "light";
   return { isDarkMode, toggleWidget };
-}
+};
+
+export const ToggleButton = () => {
+  const { isDarkMode } = useDarkMode();
+  const toggleButton =
+    isDarkMode && isDarkMode === true
+      ? "dark:bg-orange dark:text-white dark:hover:bg-white dark:hover:text-black dark:focus:bg-white dark:focus:text-orange"
+      : "text-white bg-black hover:bg-orange hover:text-black focus:bg-black focus:text-orange";
+  return { toggleButton };
+};
