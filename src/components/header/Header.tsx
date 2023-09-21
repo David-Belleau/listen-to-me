b9158c40bcd12ea9959d612716411299c69e4a0d
David Belleau <david-belleau@hotmail.com>
2023-09-21 19:31:54 +0200

    feat/add ternary

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,45 @@
 import { MdDarkMode, MdLightMode } from "react-icons/md";
-import { ToggleBg, ToggleLogo } from '../../utils/darkMode';
+import { ToggleBg, ToggleText } from "../../utils/darkMode";
+import { LuHeadphones } from "react-icons/lu";
 
 export const Header = () => {
   const { isDarkMode, toggleMode } = ToggleBg();
+  const { toggleText } = ToggleText();
 
   return (
-    <div className="mx-auto max-w-full px-4 sm:px-6">
+    <div className="px-4 sm:px-6">
       <div className="flex items-center justify-between py-4 md:space-x-10">
         <div className="flex justify-start">
-          <ToggleLogo/>
+          {isDarkMode && isDarkMode === true ? (
+            <a href="/">
+              <div className="flex flex-col justify-center items-center w-30">
+                <div className="text-orange h-10 border-4 rounded-full p-2">
+                  <LuHeadphones className="text-orange w-15" />
+                </div>
+                <p className={`font-black ${toggleText}`}>Listen to me</p>
+              </div>
+            </a>
+          ) : (
+            <a href="/">
+              <div className="flex flex-col justify-center items-center">
+                <div className="text-orange h-10 border-4 rounded-full p-2">
+                  <LuHeadphones className="text-orange w-15" />
+                </div>
+                <p className="font-black">Listen to me</p>
+              </div>
+            </a>
+          )}
         </div>
         <button onClick={() => toggleMode()}>
           {isDarkMode && isDarkMode === true ? (
-            <MdLightMode className="text-orange" size={30} title="light mode" aria-label="light mode"/>
+            <MdLightMode
+              className="text-orange"
+              size={30}
+              title="light mode"
+              aria-label="light mode"
+            />
           ) : (
-            <MdDarkMode className="text-orange" size={30} title="dark mode"  />
+            <MdDarkMode className="text-orange" size={30} title="dark mode" />
           )}
         </button>
       </div>
