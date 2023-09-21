16e61b40de4f317aff0b6839b2be441546b011cd
David Belleau <david-belleau@hotmail.com>
2023-09-21 16:08:37 +0200

    feat/modify import

--- src/components/products/SeparatorTemplate.tsx
+++ src/components/products/SeparatorTemplate.tsx
@@ -1,12 +1,14 @@
 import { ToggleBg } from "../../utils/darkMode";
-import { ProductsProps } from "../../utils/types";
+import { SeparatorTemplateProps } from "../../utils/types";
 
-export const SeparatorTemplate = ({children}: ProductsProps) => {
+export const SeparatorTemplate = ({ children }: SeparatorTemplateProps) => {
   const { toggleBg } = ToggleBg();
 
   return (
-    <div className='relative h-1 bg-orange my-10 text-base sm:text-xl text-center'>
-      <h2 className={`absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-3 ${toggleBg}`}>
+    <div className="relative h-1 bg-orange my-10 text-base sm:text-xl text-center">
+      <h2
+        className={`absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-3 ${toggleBg}`}
+      >
         {children}
       </h2>
     </div>
