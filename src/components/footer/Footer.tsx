9645682ac13bfd89452d6a34abcea838d6d79374
David Belleau <david-belleau@hotmail.com>
2023-09-21 19:33:38 +0200

    feat/delete icons

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,55 +1,22 @@
-import { ToggleBg, ToggleLogo } from "../../utils/darkMode";
-import { SiTwitter, SiFacebook, SiYoutube, SiInstagram } from "react-icons/si";
+import { ToggleBg, ToggleButton } from "../../utils/darkMode";
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
-  const { toggleBgFooter, toggleBgSeparator } = ToggleBg();
+  const { toggleBgFooter } = ToggleBg();
+  const { toggleButton } = ToggleButton();
 
   return (
-    <footer className={toggleBgFooter}>
-      <div className="flex flex-col sm:flex-row justify-evenly text-center sm:text-start p-4 sm:p-8 gap-6 2xl:gap-40">
-          <ToggleLogo/>
-        <div>
-          <h3 className="text-base font-medium mb-4">Liens utiles</h3>
-          <a href="/about-us">
-            <p className="italic text-sm hover:text-orange">
-              Qui sommes nous ?
-            </p>
-          </a>
-          <a href="/personal-data">
-            <p className="italic text-sm hover:text-orange">
-              Données personnelles
-            </p>
-          </a>
-        </div>
-        <div>
-          <h3 className="text-base font-medium mb-4">Suivez-nous sur</h3>
-          <div className="flex flex-row justify-center items-center sm:justify-start gap-3">
-            <a href="https://www.facebook.com">
-              <SiFacebook
-                className="w-6 text-orange"
-                title="logo de Facebook"
-              />
-            </a>
-            <a href="https://www.youtube.com">
-              <SiYoutube className="w-6 text-orange" title="logo de Youtube" />
-            </a>
-            <a href="https://www.twitter.com">
-              <SiTwitter className="w-6 text-orange" title="logo de Twitter" />
-            </a>
-            <a href="https://www.instagram.com">
-              <SiInstagram
-                className="w-6 text-orange"
-                title="logo de Instagram"
-              />
-            </a>
-          </div>
-        </div>
+    <footer className={`mt-8 ${toggleBgFooter}`}>
+      <div className="flex flex-col justiy-center items-center gap-8 py-5 ">
+        <a href="mailto:david-belleau@hotmail.com">
+          <button
+            className={`rounded-md w-full py-2 px-4 text-sm font-semibold transition-all duration-400 ease-in-out border-transparent ${toggleButton}`}
+          >
+            Contact
+          </button>
+        </a>
+        <p className="text-sm">&copy; {currentYear} Listen to me</p>
       </div>
-      <div className={`${toggleBgSeparator} h-px w-3/4 mx-auto`}></div>
-      <p className="py-4 text-center text-sm">
-        &copy; {currentYear} Listen to me
-      </p>
     </footer>
   );
 };
